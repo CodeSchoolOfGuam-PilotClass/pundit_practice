c4043fe0dd31e26eda4ba6c1902bba2d0a3dd229
Leon Shimizu <lmshimizu@gmail.com>
2025-03-06 18:29:53 +1000

    A lot of stuff, sorry - probably pundit stuff, controllers, etc.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  post '/signup', to: 'users#create'
+  post '/login', to: 'sessions#create'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get '/products' => 'products#index'
+  get '/products/:id' => 'products#show'
+  post '/products' => 'products#create'
+  patch '/products/:id' => 'products#update'
+  delete '/products/:id' => 'products#destroy'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  patch '/promote' => 'users#promote'
 end
